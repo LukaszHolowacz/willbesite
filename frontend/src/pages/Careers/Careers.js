c5a64fd29d76a727f3cc510410ee566cfb34950c
LukaszHolowacz <lukasz.holowacz5@gmail.com>
2025-03-29 13:41:54 +0100

    ended careers page

--- frontend/src/pages/Careers/Careers.js
+++ frontend/src/pages/Careers/Careers.js
@@ -10,9 +10,31 @@ const Careers = () => {
       <div className="careers-content">
         <h1>{translations.careers.heading}</h1>
         <p>{translations.careers.subtext}</p>
+
+        <div className="careers-icons">
+          <div>
+            👨‍💻<span>Frontend</span>
+          </div>
+          <div>
+            🧠<span>UX/UI</span>
+          </div>
+          <div>
+            🛠️<span>Backend</span>
+          </div>
+          <div>
+            📣<span>Marketing</span>
+          </div>
+          <div>
+            🧪<span>Tester</span>
+          </div>
+        </div>
+
+        <button className="apply-btn">
+          {translations.careers.button}
+        </button>
       </div>
     </div>
   );
 };
-    
+
 export default Careers;
