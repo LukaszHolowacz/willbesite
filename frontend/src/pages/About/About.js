523fa1c70970fcc8505f5c55bad40c390fb37403
LukaszHolowacz <lukasz.holowacz5@gmail.com>
2025-03-30 18:25:57 +0200

    Wersja kontrolna

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,13 +1,28 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from '../../context/LanguageContext';
+import './About.css';
 
 const About = () => {
   const { translations } = useContext(LanguageContext);
 
   return (
-    <section className="container mt-4">
-      <h1>{translations.about.heading}</h1>
-      <p>{translations.about.subtext}</p>
+    <section className="about-section">
+      <div className="about-container">
+        <div className="about-text">
+          <h1>{translations.about.heading}</h1>
+          <p className="about-subtext">{translations.about.subtext}</p>
+          <div className="about-description">
+            <p>{translations.about.description1}</p>
+            <p>{translations.about.description2}</p>
+            <p>{translations.about.description3}</p>
+            <p>{translations.about.description4}</p>
+            <p>{translations.about.description5}</p>
+          </div>
+        </div>
+        <div className="about-image">
+          <img src="/team-photo.jpg" alt="Zespół WillBeSite" />
+        </div>
+      </div>
     </section>
   );
 };
